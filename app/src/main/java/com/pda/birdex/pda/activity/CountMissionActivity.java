bdd57b46a3f981eb11382813709ec77f6e349629
zhuangchuming <zhuangchuming@birdex.cn>
2016-06-23 12:24:00 +0800

    1、增加通用的commonItemEntity; 2、完善清点任务页面

--- app/src/main/java/com/pda/birdex/pda/activity/CountMissionActivity.java
+++ app/src/main/java/com/pda/birdex/pda/activity/CountMissionActivity.java
@@ -1,37 +1,37 @@
 package com.pda.birdex.pda.activity;
 
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.net.Uri;
 import android.view.KeyEvent;
 import android.view.inputmethod.EditorInfo;
 import android.widget.TextView;
 
 import com.pda.birdex.pda.R;
-import com.pda.birdex.pda.adapter.IndexAdapter;
-import com.pda.birdex.pda.interfaces.OnRecycleViewItemClickListener;
+import com.pda.birdex.pda.fragments.BaseFragment;
+import com.pda.birdex.pda.fragments.CountMissionBussinessOtherFragment;
+import com.pda.birdex.pda.fragments.CountMissionBussniessFragment;
+import com.pda.birdex.pda.interfaces.BackHandledInterface;
+import com.pda.birdex.pda.interfaces.TitleBarBackInterface;
+import com.pda.birdex.pda.utils.HideSoftKeyboardUtil;
 import com.pda.birdex.pda.widget.ClearEditText;
 import com.pda.birdex.pda.widget.TitleView;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
 
 import butterknife.Bind;
 
 /**
  * Created by chuming.zhuang on 2016/6/22.
  */
-public class CountMissionActivity extends BarScanActivity {
+public class CountMissionActivity extends BarScanActivity implements BaseFragment.OnFragmentInteractionListener, BackHandledInterface {
 
     @Bind(R.id.title)
     TitleView title;
     @Bind(R.id.edt_input_business)
     ClearEditText edt_input_business;
-    @Bind(R.id.rcy)
-    RecyclerView rcy;
 
-    IndexAdapter adapter;
-    List<String> indexList;
-    String []alist ={"格格家","美囤妈妈","网易惠惠","懂球帝","其他"};
+    CountMissionBussinessOtherFragment otherFragment;
+    CountMissionBussniessFragment bussniessFragment;
 
     @Override
     public int getbarContentLayoutResId() {
@@ -40,12 +40,18 @@ public class CountMissionActivity extends BarScanActivity {
 
     @Override
     public void barInitializeContentViews() {
-
-        title.setTitle("清点任务");
-        indexList = new ArrayList<>();
-        for(int i =0 ; i<alist.length;i++){
-            indexList.add(alist[i]);
-        }
+        EventBus.getDefault().register(this);
+        title.setTitle(getString(R.string.count_task));
+        title.setBackInterface(new TitleBarBackInterface() {//
+            @Override
+            public void onBackClick() {
+                if(getSupportFragmentManager().getBackStackEntryCount()>0)
+                    getSupportFragmentManager().popBackStack();
+                else{
+                    CountMissionActivity.this.finish();
+                }
+            }
+        });
         edt_input_business.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
@@ -58,19 +64,12 @@ public class CountMissionActivity extends BarScanActivity {
                 return false;
             }
         });
-        adapter = new IndexAdapter(this,indexList);
-        rcy.setLayoutManager(new GridLayoutManager(this,2));
-        rcy.setAdapter(adapter);
-        adapter.setOnRecycleViewItemClickListener(new OnRecycleViewItemClickListener() {
-            @Override
-            public void onItemClick(int position) {
-                if (position == 5){
-
-                }else{
-
-                }
-            }
-        });
+        if (otherFragment == null)
+            otherFragment = new CountMissionBussinessOtherFragment();
+        if (bussniessFragment == null) {
+            bussniessFragment = new CountMissionBussniessFragment();
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, bussniessFragment).commit();
     }
 
     @Override
@@ -80,6 +79,29 @@ public class CountMissionActivity extends BarScanActivity {
 
     @Override
     public void ClearEditTextCallBack(String code) {
+        HideSoftKeyboardUtil.hideSoftKeyboard(this);
+    }
 
+    @Override
+    public void setSelectedFragment(BaseFragment selectedFragment) {
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
+    }
+    @Subscribe
+    public void onEvent(String count_mission_other){
+        if (count_mission_other.equals("count_mission_other")) {
+            if (!otherFragment.isAdded())
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.in_from_right,R.anim.out_from_left,R.anim.in_from_left,R.anim.out_from_right).replace(R.id.framelayout, otherFragment).addToBackStack("").commit();
+        }
     }
 }
